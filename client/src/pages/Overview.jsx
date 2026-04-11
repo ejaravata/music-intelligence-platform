bdd658349dc747f3c3ac1da5e90ba31e618f3d9d
Lexi Bobb <lcbobb02@yahoo.com>
2026-04-11 18:30:53 -0500

    Added file for overview page

---
+++ client/src/pages/Overview.jsx
@@ -0,0 +1,8 @@
+import { useState, useEffect } from 'react';
+import Header from '../components/Header.jsx';
+import SideMenu from '../components/SideMenu.jsx';
+import config from '../config.json';
+
+export default function Overview() {
+  return <h1>Overview</h1>;
+}
