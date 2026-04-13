8d76bf40ce1254f48ba16221370c3ee36b48f2f8
yujeremy21 <yujeremy@seas.upenn.edu>
2026-04-13 00:57:18 -0400

    Adding path for GrammyAnalytics and BillboardAnalytics

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Overview from "./pages/Overview.jsx";
-import Analytics from "./pages/Analytics.jsx";
+import GrammyAnalytics from "./pages/GrammyAnalytics.jsx";
+import BillboardAnalytics from "./pages/BillboardAnalytics.jsx";
 import Favorites from "./pages/Favorites.jsx";
 import SongInfo from "./pages/SongInfo.jsx";
 import ArtistInfo from "./pages/ArtistInfo.jsx";
 
+// added path for both analytics and grammy-analytics since had some issues with 
+// the path, bc it didn't connect properly. 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/overview" element={<Overview />} />
-        <Route path="/analytics" element={<Analytics />} />
+        <Route path="/analytics" element={<GrammyAnalytics />} />
+        <Route path="/grammy-analytics" element={<GrammyAnalytics />} />
+        <Route path="/billboard-analytics" element={<BillboardAnalytics />} />
         <Route path="/favorites" element={<Favorites />} />
         <Route path="/song/:songId" element={<SongInfo />} />
         <Route path="/artist/:artistId" element={<ArtistInfo />} />
